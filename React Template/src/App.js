7412a31cb754060ebb07280657dc6a8f17219ead
Adonayg <adonay@tutanota.com>
2019-02-05 20:17:04 +0000

    React template

---
+++ React Template/src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div class="header">
+          <h1>App</h1>
+          <p>React template</p>
+        </div>
+        <div class="container">
+
+        </div>
+        <footer class="footer">
+          <p>&copy; 2019 Application, Inc.</p>
+        </footer>
+        <script src="js/main.js"></script>
+      </div>
+    );
+  }
+}
+
+export default App;
